fd8c57dfc6bde6a8f2be384bbe26717ca39a4a36
Andrew Booker <improvizone@gmail.com>
2023-10-02 23:22:19 +0100

    extending python server to support two ports each playing a different sound

--- py/channel_synth.py
+++ py/channel_synth.py
@@ -5,27 +5,25 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import pygame as pg
 import time
 import sys
+import threading
+import readchar
 
 
 class Player:
-    def __init__(self):
-        pg.mixer.init(frequency=44100, size=-16, channels=2, buffer=1024)
-        pg.init()
+    def __init__(self, vol, sound):
         self.channel = pg.mixer.find_channel()
-        self.channel.set_volume(1.0)
-        self.sound = pg.mixer.Sound(sys.argv[1])
-
-    def __del__(self):
-        pg.quit()
+        self.channel.set_volume(vol[0], vol[1])
+        self.sound = pg.mixer.Sound(sound)
 
     def play(self):
         self.channel.play(self.sound)
 
 
-player = Player()
-
-
 class Controller(BaseHTTPRequestHandler):
+    def __init__(self, vol, sound, *args):
+        self.player = Player(vol, sound)
+        BaseHTTPRequestHandler.__init__(self, *args)
+
     def _standardResponse(self):
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
@@ -38,9 +36,47 @@ class Controller(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_POST(self):
-        player.play()
+        self.player.play()
         self._standardResponse()
+        self.end_headers()
+
+class PG:
+    def __init__(self):
+        pg.mixer.init(frequency=44100, size=-16, channels=2, buffer=1024)
+        pg.init()
+
+    def __del__(self):
+        pg.quit()
+
+ignore = PG()
+
+class ControllerA(Controller):
+    def __init__(self, *args):
+        Controller.__init__(self, (1.0, 0.0), sys.argv[1], *args)
+
+class ControllerB(Controller):
+    def __init__(self, *args):
+        Controller.__init__(self, (0.0, 1.0), sys.argv[2], *args)
 
 
-HTTPServer(("0.0.0.0", 9900), Controller).serve_forever()
+servers = [
+    HTTPServer(("0.0.0.0", 9900), ControllerA),
+    HTTPServer(("0.0.0.0", 9901), ControllerB)
+]
+
+
+shouldStop = threading.Event()
+threads = [threading.Thread(target=s.serve_forever, args=(), daemon=True) for s in servers]
+[t.start() for t in threads]
+
+print("press 'q' to exit")
+while not shouldStop.is_set():
+    c = readchar.readchar()
+    if c == "q":
+        print("stopping...")
+        shouldStop.set()
+        [s.shutdown() for s in servers]
+        [s.server_close() for s in servers]
+        [t.join() for t in threads]
+
 
